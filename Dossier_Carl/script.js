a8dba0009bc20944a6251abb9adb193183781282
Carl <carlos.premi@gmail.com>
2023-11-24 12:22:42 +0100

    commit

--- Dossier_Carl/script.js
+++ Dossier_Carl/script.js
@@ -3,3 +3,17 @@
 3 4 6 Clément
 */
 
+/*
+
+ED level :  
+
+"Primary/elementary school"
+"Secondary school (e.g. American high school, German Realschule or Gymnasium, etc.)"
+"Some college/university study without earning a degree"
+"Associate degree (A.A., A.S., etc.)"
+"Bachelor’s degree (B.A., B.S., B.Eng., etc.)"
+"Master’s degree (M.A., M.S., M.Eng., MBA, etc.)"
+"Professional degree (JD, MD, Ph.D, Ed.D, etc.)"
+"Something else"
+
+*/
